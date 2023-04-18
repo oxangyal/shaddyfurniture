649694b6cb0cf0a97b69782205b43f8bf6e3a540
oxangyal <Oxana.mitch@gmail.com>
2023-04-17 23:59:54 -0400

    Added slider via swiper

---
+++ js/index.js
@@ -0,0 +1,11 @@
+new Swiper("#swiper-1", {
+    effect: "fade",
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false
+    },
+    pagination: {
+        el: "#swiper-1 .swiper-pagination",
+        clickable: true
+    }
+});
